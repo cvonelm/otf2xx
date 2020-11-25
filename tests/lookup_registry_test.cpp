f61054efe94d0ad7ed92557e02d6260a49bd1e8c
Christian von Elm <cvonelm@cvonelm.de>
2020-11-25 20:24:40 +0100

    minimal example for user defined type

--- tests/lookup_registry_test.cpp
+++ tests/lookup_registry_test.cpp
@@ -2,6 +2,89 @@
 
 #include <iostream>
 #include <string>
+#include <unistd.h>
+
+enum class LocationType
+{
+    THREAD,
+    CPU,
+    SAMPLE_CPU,
+    UNKOWN
+};
+
+struct Location
+{
+    LocationType type;
+    int location;
+
+    Location() : type(LocationType::UNKOWN), location(-1)
+    {
+    }
+
+    Location(LocationType t, int l) : type(t), location(l)
+    {
+    }
+
+    std::string name() const
+    {
+        return "name";
+    }
+
+    pid_t tid() const
+    {
+        return (type == LocationType::THREAD ? location : -1);
+    }
+    int cpuid() const
+    {
+        return (type != LocationType::THREAD ? location : -1);
+    }
+
+    // Needed becausse this is used as a Key in some Structures.
+    // Simply order (arbitrarly) by type first, then by location
+    friend bool operator<(const Location& lhs, const Location& rhs)
+    {
+        if (lhs.type != rhs.type)
+        {
+            return lhs.type < rhs.type;
+        }
+        else
+        {
+            return lhs.location < rhs.location;
+        }
+    }
+};
+
+struct CpuLocation : public Location
+{
+    CpuLocation(int cpuid) : Location(LocationType::CPU, cpuid)
+    {
+    }
+};
+
+struct ThreadLocation : public Location
+{
+    ThreadLocation(pid_t tid) : Location(LocationType::THREAD, tid)
+    {
+    }
+};
+
+struct SampleCpuLocation : public Location
+{
+    SampleCpuLocation(int cpuid) : Location(LocationType::SAMPLE_CPU, cpuid)
+    {
+    }
+};
+
+struct ByLocation
+{
+    using key_type = Location;
+
+    key_type key;
+
+    explicit ByLocation(key_type key) : key(key)
+    {
+    }
+};
 
 struct ByCPU
 {
@@ -34,7 +117,8 @@ struct MyHolder
 template <>
 struct MyHolder<otf2::definition::string>
 {
-    using type = otf2::lookup_definition_holder<otf2::definition::string, ByCPU, ByProcess>;
+    using type =
+        otf2::lookup_definition_holder<otf2::definition::string, ByCPU, ByProcess, ByLocation>;
 };
 template <>
 struct MyHolder<otf2::definition::system_tree_node>
@@ -42,7 +126,6 @@ struct MyHolder<otf2::definition::system_tree_node>
     using type =
         otf2::lookup_definition_holder<otf2::definition::system_tree_node, ByCPU, ByProcess>;
 };
-
 int main()
 {
     otf2::registry rego;
@@ -53,6 +136,9 @@ int main()
 
     // otf2::registry_view<otf2::definition::string, ByCPU> cpu_strings(rego);
 
+    rego.emplace<otf2::definition::string>(ByLocation(CpuLocation(42)), std::string("The answer"));
+    rego.emplace<otf2::definition::string>(ByLocation(ThreadLocation(666)),
+                                           std::string("Number of the beast"));
     const auto& str1 = reg.create<otf2::definition::string>(ByCPU(7), std::string("cpu 7"));
     const auto& str2 =
         reg.create<otf2::definition::string>(ByProcess(7435), std::string("process 7435"));
